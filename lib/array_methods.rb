6967942e6eac301f4b380919c1bfe44c33ed4a77
TShipley <taneymail@gmail.com>
2020-02-01 03:39:03 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-
+ array.include?(element)
 end
 
 def using_sort(array)
-
+  using_sort = array.sort
 end
 
 def using_reverse(array)
-
+  using_reverse = array.reverse
 end
 
 def using_first(array)
-
+ using_first = array.first
 end
 
 def using_last(array)
-
+  using_last = array.last
 end
 
 def using_size(array)
-
+  using_size = array.size
 end
